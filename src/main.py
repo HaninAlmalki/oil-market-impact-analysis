29ac5f9622e5c4342244c5eb979f82d221d19426
HaninAlmalki <haninooalmalki@gmail.com>
2026-04-10 15:57:46 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Oil Market Analysis Project")
+from data_loader import load_oil_data
+
+def main():
+    oil = load_oil_data()
+    print(oil.head())
+
+if __name__ == "__main__":
+    main()
