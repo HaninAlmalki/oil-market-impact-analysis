0e85601bfa1566c9bdabb7e89eb6afe759d34f23
HaninAlmalki <haninooalmalki@gmail.com>
2026-04-14 20:09:05 +0300

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,10 @@
 import yfinance as yf
+import pandas as pd
 
 def load_oil_data():
     oil = yf.download("BZ=F", start="2015-01-01")
+
+    if isinstance(oil.columns, pd.MultiIndex):
+        oil.columns = oil.columns.get_level_values(0)
+
     return oil
